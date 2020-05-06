914ca44b1ff2688bf6fc3e1bd9c1a1c98e8edf44
mei0225 <mei300225@gmail.com>
2020-05-06 22:48:06 +0900

    スクロール時に下方からメンバー画像をフェードインする動作を付与

--- js/script.js
+++ js/script.js
@@ -9,4 +9,16 @@ $(function(){
 
     /*テキストフェードイン*/
     $(window).fadeThis();
+
+    /*メンバー画像フェードイン*/
+    $(window).scroll(function (){
+      $('.fadein').each(function(){
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight + 200){
+          $(this).addClass('scrollin');
+        }
+      });
+    });
   });
