8ec2349ee8039ec3c440aa5d73ee86e19f3ca37f
mei0225 <mei300225@gmail.com>
2020-05-06 23:50:44 +0900

    スムーズスクロールを実装

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ $(function(){
     $('.slider').bxSlider({
       auto: "true",
       mode: "fade",
-      randomStart: "true"
     });
 
     /*テキストフェードイン*/
@@ -20,5 +19,14 @@ $(function(){
           $(this).addClass('scrollin');
         }
       });
+    });
+
+    $('a[href^="#"]').click(function(){
+      var speed = 1500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
     });
   });
