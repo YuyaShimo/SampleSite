c033cc784f1134d86f35027b0109d4184b6ea71a
mei0225 <mei300225@gmail.com>
2020-05-06 22:24:17 +0900

    メインイメージにテキストを挿入 h2タグにスライドを付与

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 $(function(){
+
+    /*メインイメージスライダー*/ 
     $('.slider').bxSlider({
       auto: "true",
       mode: "fade",
       randomStart: "true"
     });
+
+    /*テキストフェードイン*/
+    $(window).fadeThis();
   });
