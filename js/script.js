b4b51cf20782633f7c08dfc3fda975fad0eb9161
mei0225 <mei300225@gmail.com>
2020-05-13 23:13:19 +0900

    変数を変更

--- js/script.js
+++ js/script.js
@@ -8,12 +8,13 @@ $(function(){
 
     /*スマホナビゲーションチェックボックスを外す処理*/
     let unlockCheckBox = function() {
-      let $trigger = $('.header__navi a');
+      let $trigger = $('.header-navi a');
       let $target = $('input[type="checkbox"]');
       $trigger.click(function() {
-          $target.prop('checked', false);
+        $target.prop('checked', false);
       });
-  };
+    };
+    unlockCheckBox();
 
     /*テキストフェードイン*/
     $(window).fadeThis();
@@ -21,9 +22,9 @@ $(function(){
     /*メンバー画像フェードイン*/
     $(window).scroll(function (){
       $('.fadein').each(function(){
-        var elemPos = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
+        let elemPos = $(this).offset().top;
+        let scroll = $(window).scrollTop();
+        let windowHeight = $(window).height();
         if (scroll > elemPos - windowHeight + 200){
           $(this).addClass('scrollin');
         }
@@ -32,10 +33,10 @@ $(function(){
 
     /*スムーズスクロール*/
     $('a[href^="#"]').click(function(){
-      var speed = 1500;
-      var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
-      var position = target.offset().top;
+      let speed = 1500;
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top;
       $("html, body").animate({scrollTop:position}, speed, "swing");
       return false;
     });
