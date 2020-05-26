e81665aa8eb710e0a696c313498f35023a636633
mei0225 <mei300225@gmail.com>
2020-05-27 01:20:54 +0900

    投稿ページのレイアウト

--- single.php
+++ single.php
@@ -1,4 +1,34 @@
 <?php get_header(); ?>
-  
+<div class="page-inner full-width">
+    <div class="page-main">
+      <div class="main-container">
+        <div class="main-wrapper"> 
+<?php 
+  if( have_posts() ):
+    while( have_posts() ) : the_post();
+?>
+
+    <div class="news">
+      <time class="time"><?php the_time( 'Y.m.d' ); ?></time>
+      <p class="title"><?php the_title(); ?></p>
+      <div class="news-body"><?php the_content(); ?></div>
+    </div>
+    <div class="more-news">
+      <div class="prev">
+        <a class="another-kink" href="#">PREV</a>
+      </div>
+      <div class="next">
+        <a class="another-link" href="#">NEXT</a>
+      </div>
+    </div>
+
+<?php
+  endwhile;
+  endif;
+?>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <?php get_footer(); ?>
