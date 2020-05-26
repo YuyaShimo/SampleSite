ccc7c422fb86ac39dce955591064f43911738d20
mei0225 <mei300225@gmail.com>
2020-05-27 00:27:52 +0900

    投稿の詳細ページの際はカテゴリー名を表示する

--- functions.php
+++ functions.php
@@ -6,3 +6,13 @@ register_nav_menus(
     'place_footer' => 'フッターナビ',
   )
 );
+
+//投稿ページの際はカテゴリー名を表示する
+function get_main_title() {
+  if( is_singular( 'post') ):
+    $category_obj = get_the_category();
+    return $category_obj[0]->name;
+  elseif( is_page() ):
+    return get_the_title();
+  endif;
+}
