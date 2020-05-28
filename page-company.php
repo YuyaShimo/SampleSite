a346a1adf5df8c7e0a01cd7e74c2a2f6cd650fa5
mei0225 <mei300225@gmail.com>
2020-05-28 23:24:32 +0900

    削除した部分は別ファイルで管理し、読み込んで利用

--- page-company.php
+++ page-company.php
@@ -5,34 +5,13 @@
     <div class="page-inner">
       <div class="page-main">
         <ul class="commons">
-        <?php
-  $parent_id = get_the_ID();
-  $args = array(
-    'posts_per_page' => -1,
-    'post_type' => 'page',
-    'orderby' => 'menu_order',
-    'order' => 'ASC',
-    'post_parent' => $parent_id,
-  );
-  $common_pages = new WP_Query( $args );
+<?php
+  $common_pages = get_child_pages();
   if( $common_pages->have_posts() ):
     while( $common_pages->have_posts() ): $common_pages->the_post();
-?>
-          <li class="common-item">
-            <a class="common-link" href="<?php the_permalink(); ?>">
-              <div class="common-image"><?php the_posst_thumbnail(); ?></div>
-                <div class="common-body">
-                  <p class="name"><?php the_title(); ?></p>
-                  <p class="caption"><?php echo get_the_title(); ?></p>
-                </div>
-                <div class="btn button-box">
-                  <button type="button" class="see-detail">MORE >></button>
-                </div>
-            </a>
-          </li>
-          <?php
-  endwhile;
-  wp_reset_postdata();
+      get_template_part( 'content-common');
+    endwhile;
+    wp_reset_postdata();
   endif;
 ?>         
         </ul>
