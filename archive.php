5f14816576861e4cbabfa6a4187ee13ae57f602b
mei0225 <mei300225@gmail.com>
2020-06-03 01:38:10 +0900

    ページャーのプラグイン Prime Strategy Pagye Navi を有効化 ページ選択時に背景色と文字が変化するよう実装

--- archive.php
+++ archive.php
@@ -14,6 +14,15 @@
 ?>
              
           </div>
+          <div class="pager">
+            <ul class="pagerlist">
+<?php
+if( function_exists( 'page_navi' )):
+  page_navi();
+endif;
+?>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
