084909c4d72a4ad14d23ebf2e8c10ed23e53a357
mei0225 <mei300225@gmail.com>
2020-06-03 23:06:54 +0900

    記事の移動ができるように、投稿ページ間のナビゲーションを設置

--- content-single.php
+++ content-single.php
@@ -1,13 +1,23 @@
-<div class="news">
-  <time class="time"><?php the_time( 'Y.m.d' ); ?></time>
-  <p class="title"><?php the_title(); ?></p>
-  <div class="news-body"><?php the_content(); ?></div>
-</div>
-<div class="more-news">
-  <div class="prev">
-    <a class="another-kink" href="#">PREV</a>
+  <div class="news">
+    <time class="time"><?php the_time( 'Y.m.d' ); ?></time>
+    <p class="title"><?php the_title(); ?></p>
+    <div class="news-body"><?php the_content(); ?></div>
   </div>
-  <div class="next">
-    <a class="another-link" href="#">NEXT</a>
+  <div class="more-news">
+<?php
+$next_post = get_next_post();
+$prev_post = get_previous_post();
+if( $prev_post ):
+?>
+    <div class="prev">
+      <a class="another-kink" href="<?php echo get_permalink( $prev_post->ID ); ?> ">PREV</a>
+    </div>
+<?php
+endif;
+if( $next_post):
+?>
+    <div class="next">
+      <a class="another-link" href="<?php echo get_permalink( $next_post->ID ); ?>">NEXT</a>
+    </div>
+<?php endif; ?>
   </div>
-</div>
